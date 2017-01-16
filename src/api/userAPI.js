4c614294007005396d244e5f53ad0d61db32ccfb
Alex Clark <gac208@gmail.com>
2017-01-15 21:38:18 -0500

    added delete function for user

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -11,6 +11,19 @@ function get(url) {
   return fetch(baseUrl + url).then(onSuccess, onError);
 }
 
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
+// Can't call func delete since reserved word.
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
+}
+
 function onSuccess(response) {
   return response.json();
 }
